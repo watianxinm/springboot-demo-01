e26eb7d9ad81f9e5e8433e9b438e60006e093292
watianxinm <2364671412@qq.com>
2021-10-15 17:57:51 +0800

    1.设置druid过滤 2.设置完成后没有进行测试

--- src/main/java/com/study/demo01web/Config/DruidConfig.java
+++ src/main/java/com/study/demo01web/Config/DruidConfig.java
@@ -1,8 +1,11 @@
 package com.study.demo01web.Config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewFilter;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,27 +23,34 @@ public class DruidConfig {
         return new DruidDataSource();
     }
 
-    //后台监控
-    //该方法相当于web.xml
+    //后台监控  该方法相当于web.xml
+    //因为SpringBoot内置了servlet容器，所以没有web.xml文件，则用替代方法：注入ServletRegistrationBean
     @Bean
     public ServletRegistrationBean statViewServlet(){
         ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(),"/druid/*");
-
         //后台需要有人登陆：账号密码配置
         HashMap<String, String> initParameters = new HashMap<>();
-
         //增加配置
         initParameters.put("loginUsername","admin");
         initParameters.put("loginPassword","123456");
-
         //设置允许谁可以访问
         initParameters.put("allow","");
-
         //禁止谁能访问 initParameters.put("kuangshen","192.168.11.123");
-
         //设置初始化参数
         bean.setInitParameters(initParameters);
+        return bean;
+    }
 
+    //filter
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean<WebStatFilter> bean = new FilterRegistrationBean<>(new WebStatFilter());
+//        FilterRegistrationBean bean = new FilterRegistrationBean();
+//        bean.setFilter(new WebStatFilter());
+        HashMap<String, String> initParameters = new HashMap<>();
+        //不过滤的路径
+        initParameters.put("exclusions","*.js, *.css, druid/*");
+        bean.setInitParameters(initParameters);
         return bean;
     }
 }
