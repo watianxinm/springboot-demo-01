8d1aab69ae545debe5632043fbdf8a7af99a03a3
watianxinm <2364671412@qq.com>
2021-11-18 10:36:36 +0800

    整理笔记 加了一些注释

--- src/main/java/com/study/demo01web/Config/DruidConfig.java
+++ src/main/java/com/study/demo01web/Config/DruidConfig.java
@@ -1,7 +1,6 @@
 package com.study.demo01web.Config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewFilter;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
